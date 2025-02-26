1d77d3d5317ce71f530fa525ae17a3fd933a4eb9
P202U <156369604+P202U@users.noreply.github.com.>
2025-02-26 23:07:23 +0530

    Complete comment routes

--- backend/src/controllers/comment.controller.ts
+++ backend/src/controllers/comment.controller.ts
@@ -2,70 +2,91 @@ import prisma from "@prismaClient";
 import { Request, Response } from "express-serve-static-core";
 
 interface commentRequestBody {
-    userId: string;
-    movieId: string;
-    content: string;
-    parentId?: string;
+  userId: string;
+  movieId: string;
+  content: string;
+  parentId?: string;
 }
 
 const createComment = async (req: Request<{}, {}, commentRequestBody>, res: Response): Promise<any> => {
-    const { userId, movieId, content, parentId } = req.body;
+  const { userId, movieId, content, parentId } = req.body;
 
-    try {
-        const newComment = await prisma.comment.create({
-            data: {
-                userId,
-                movieId,
-                content,
-                parentId: parentId || null,
-            }
-        })
+  try {
+    const newComment = await prisma.comment.create({
+      data: {
+        userId,
+        movieId,
+        content,
+        parentId: parentId || null,
+      }
+    })
 
-        return res.status(201).json(newComment);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error("Error:", error.message);
-            return res.status(500).json({ message: "Error creating comment", error: error.message });
-        } else {
-            console.error("An unknown error occurred.");
-            return res.status(500).json({ message: "An unknown error occurred" });
-        }
+    return res.status(201).json({ message: 'Comment succesfully added', newComment });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+      return res.status(500).json({ message: "Error creating comment", error: error.message });
+    } else {
+      console.error("An unknown error occurred.");
+      return res.status(500).json({ message: "An unknown error occurred" });
     }
+  }
 }
 
 const getComments = async (req: Request, res: Response): Promise<any> => {
-    const { movieId } = req.params;
-  
-    try {
-      const comments = await prisma.comment.findMany({
-        where: {
-          movieId: movieId,
-          parentId: null, 
-        },
-        include: {
-          user: true,
-          replies: {     
-            include: {
-              user: true,
-            },
+  const { movieId } = req.params;
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        movieId: movieId,
+        parentId: null,
+      },
+      include: {
+        user: true,
+        replies: {
+          include: {
+            user: true,
           },
         },
-        orderBy: {
-          createdAt: 'asc', 
-        },
-      });
-  
-      return res.json(comments);
-    } catch (error: unknown) {
-        if (error instanceof Error) {
-            console.error("Error:", error.message);
-            return res.status(500).json({ message: "Error creating comment", error: error.message });
-        } else {
-            console.error("An unknown error occurred.");
-            return res.status(500).json({ message: "An unknown error occurred" });
-        }
-    }
-  };
-  
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
 
-export { createComment, getComments };
+    return res.json({ message: 'Comments succesfully retrived', comments });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+      return res.status(500).json({ message: "Error creating comment", error: error.message });
+    } else {
+      console.error("An unknown error occurred.");
+      return res.status(500).json({ message: "An unknown error occurred" });
+    }
+  }
+};
+
+const deleteComment = async (req: Request, res: Response): Promise<any> => {
+  const { commentId } = req.params;
+
+  const deletedComment = await prisma.comment.update({
+    where: { id: commentId },
+    data: { deleted: true },
+  });
+
+  return res.json(deletedComment);
+}
+
+const editComment = async (req: Request, res: Response): Promise<any> => {
+  const {commentId, content} = req.body;
+
+  const editedComment = await prisma.comment.update({
+    where: {id: commentId},
+    data: {content: content}
+  })
+
+  return res.json(editedComment);
+}
+
+export { createComment, getComments, deleteComment, editComment };
--- backend/src/routes/comments.routes.ts
+++ backend/src/routes/comments.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createComment, getComments } from '@controllers/comment.controller';
+import { createComment, getComments, deleteComment, editComment } from '@controllers/comment.controller';
 import { authenticateJWT } from '@middleware/jwtAuth.middleware';
 
 const router = express.Router();
 
 router.post('/create-comment', authenticateJWT, createComment);
-router.get('/:movieId', getComments);
+router.get('/:movieId', authenticateJWT, getComments);
+router.delete('/:commentId', authenticateJWT, deleteComment);
+router.patch('/:commentId', authenticateJWT, editComment);
 
 export default router;
